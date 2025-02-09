152443c6e0ec0ddbf15f6f9dea6dd187fab79969
Alex <91841844+avirt1274@users.noreply.github.com>
2025-02-09 19:12:33 +0300

    g

--- main.mjs
+++
@@ -1,9 +0,0 @@
-import virustotal from '@api/virustotal';
-
-const API = "15fe05f4310bcacd4e669d1abe109cfbb86e11f5ebe7a3b5703749c1227a5021";
-
-virustotal.postFiles({file: 'server.sh'}, {
-  'x-apikey': API
-})
-  .then(({ data }) => console.log(data))
-  .catch(err => console.error(err));
